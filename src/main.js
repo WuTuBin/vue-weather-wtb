4fdc13b464ad1d7aa2630db25a9a3079fe8c1ca3
wutubin <807941247@qq.com>
2018-09-13 14:22:13 +0800

    '初次提交'

---
+++ src/main.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import Resource from 'vue-resource'
+import payKeyboard from 'my-pay-keyboard'
+Vue.config.productionTip = false
+Vue.use(ElementUI)
+Vue.use(Resource)
+Vue.use(payKeyboard)
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  render: h => h(App)
+})
